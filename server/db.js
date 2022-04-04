c165bb4bf58e746f6c1aab98d41620811c1845bb
Peter <petermada@gmail.com>
2022-04-04 09:27:56 +0200

    Remove comment

--- server/db.js
+++ server/db.js
@@ -5,7 +5,7 @@ const pool = new Pool({
   user: process.env.PG_USER,
   password: process.env.PG_PASSWORD,
   host: process.env.PG_HOST,
-  port: process.env.PG_PORT, //automatic run on this port
+  port: process.env.PG_PORT,
   database: process.env.PG_DATABASE,
   ssl: {
     rejectUnauthorized: false,
